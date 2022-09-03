195bddb79f7477843e14372fb22bc2ebd0af6e03
Bektur777 <ubei003@mail.ru>
2022-09-03 10:11:54 +0600

    adding admin to quiz

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['text', 'draft']
+    list_editable = ['draft', ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'text', 'answer']
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category']
+    save_as = True
+    save_on_top = True
+    prepopulated_fields = {'slug': ('title',)}
