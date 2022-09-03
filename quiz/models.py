49e88b290b36fc97c8b91f6ce69bdd02608f0bb9
Bektur777 <ubei003@mail.ru>
2022-09-03 09:59:07 +0600

    adding models to quiz app

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField('Имя', max_length=25)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Answer(models.Model):
+    text = models.TextField('Ответ')
+    draft = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+
+class Question(models.Model):
+    title = models.CharField('Название', max_length=25)
+    question = models.TextField('Вопрос')
+    answer = models.ForeignKey(Answer, related_name='answer', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class Quiz(models.Model):
+    title = models.CharField('Название', max_length=25)
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='question', on_delete=models.CASCADE)
+    slug = models.BooleanField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
